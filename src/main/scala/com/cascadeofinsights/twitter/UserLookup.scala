91cd652bf7b90fc8362bca53eb781a00d64afecf
Adam Bell <agbell@gmail.com>
2018-12-08 19:42:22 -0500

    twitter caching continues

--- src/main/scala/com/cascadeofinsights/twitter/UserLookup.scala
+++ src/main/scala/com/cascadeofinsights/twitter/UserLookup.scala
@@ -7,13 +7,17 @@ import scalaz.zio.IO
 import com.danielasfregola.twitter4s.TwitterRestClient
 import scalaz._
 import Scalaz._
+import com.cascadeofinsights.twitter.UserLookup.cache
 import com.cascadeofinsights.util.Cache
 import com.danielasfregola.twitter4s.entities.{RatedData, User}
+import org.json4s.{NoTypeHints, native}
 import scalaz.zio.interop.future._
 import scalaz.zio.IO
+import scalaz.zio.interop.scalaz72._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
+import scala.util.Try
 
 
 object UserLookup {
@@ -34,9 +38,18 @@ object UserLookup {
     lookup100MaxRaw(users).map(_.map(t => (t.name, t.followers_count)))
   }
   private def lookup100RawCached(rootPath : Path)(users: List[TwitterName]):IO[Nothing, Seq[User]] = {
-    //users.traverse(u => Cache.getFromDisk(rootPath)(u.digest))
-    ???
+    for{
+      cached <- users.filterM(t => Cache.isOnDisk(rootPath)(t.digest))
+      nonCached = (users.toSet -- cached).toList
+      cachedResults <- cached.traverse(x => Cache.getFromDisk(rootPath)(x.digest))
+          .map(_.flatten.map(toUser(_)).flatten)
+      nonCachedResults <- lookup100MaxRaw(nonCached)
+      _ <- nonCachedResults.toList.traverse(cache(rootPath)(_))
+    } yield (nonCachedResults ++ cachedResults)
   }
+
+  def cache(rootPath : Path)(user : User) : IO[Nothing,Unit] = Cache.writeToDisk(rootPath)(TwitterName(user.name).get.digest,fromUser(user))
+
   private def lookup100MaxRaw(users: List[TwitterName]): IO[Nothing, Seq[User]] = {
     val users1 = users.take(100).map(_.name)
     val f: Future[RatedData[Seq[User]]] = restClient.users(users1)
@@ -47,4 +60,9 @@ object UserLookup {
       case Right(b) => b
     }
   }
+
+  implicit val formats = native.Serialization.formats(NoTypeHints)
+
+  def toUser(s : String) : Option[User] = Try(native.Serialization.read[User](s)).toOption
+  def fromUser(u : User) : String = native.Serialization.write(u)
 }
--- src/main/scala/com/cascadeofinsights/util/Cache.scala
+++ src/main/scala/com/cascadeofinsights/util/Cache.scala
@@ -11,6 +11,13 @@ import scala.util.Try
 
 object Cache {
 
+  def isOnDisk(rootPath: Path)(digest: SHA256Hash): IO[Nothing, Boolean] = {
+    IO.sync {
+      val path = Paths.get(rootPath.toAbsolutePath.toString + "/" + digest)
+      Files.exists(path)
+    }
+  }
+
   def getFromDisk(rootPath: Path)(digest: SHA256Hash): IO[Nothing, Option[String]] = {
     IO.sync {
       val path = Paths.get(rootPath.toAbsolutePath.toString + "/" + digest)
@@ -22,7 +29,7 @@ object Cache {
     }
   }
 
-  def writeToDisk(rootPath : Path): (SHA256Hash, String) => IO[Unit, Unit] =
+  def writeToDisk(rootPath : Path): (SHA256Hash, String) => IO[Nothing, Unit] =
     (digest, html) => {
       IO.sync {
         val writer = Try(new FileWriter(new File(rootPath.toAbsolutePath.toString + "/" + digest)))
